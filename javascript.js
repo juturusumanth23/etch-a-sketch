a2704e9282221cde8b92cbf08a17dc9ca553296e
Xumanth <juturusumanth23@gmail.com>
2025-07-02 00:46:01 +0530

    Add a button to set grid size as per user input

--- javascript.js
+++ javascript.js
@@ -1,12 +1,28 @@
 const container = document.querySelector("#container");
-let divsCount = 16;
+const btn = document.querySelector("#btn");
 
-for (let i=0;i<divsCount**2;i++) {
-    const div = document.createElement("div");
-    
-    container.appendChild(div);
-    div.addEventListener("mouseenter", (e)=> {
-        e.target.style.backgroundColor="green";
-    });
-    
+btn.addEventListener("click", () => createNewGrid(getGridSize()));
+
+function getGridSize() {
+    return prompt("Enter the Grid Size");
 }
+
+function createNewGrid(gridSize) {
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (let i=0;i<gridSize**2;i++) {
+        const div = document.createElement("div");
+        
+        container.appendChild(div);
+        div.addEventListener("mouseenter", (e)=> {
+            e.target.style.backgroundColor="green";
+        });
+        
+    }
+
+}
+
+createNewGrid(16);
